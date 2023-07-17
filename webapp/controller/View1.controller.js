9e5ba66730c1159e5a6965ba14dd7fea674aa68f
hqmxlyx <937425049@qq.com>
2023-07-17 14:26:26 +0800

    Initial commit
    Change-Id: Ifae2a1cb310c49dab172c6cfcd82fb06945fe4df

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("git_appgit_app.controller.View1", {
+	onInit: function() {
+		var url = "/sap/opu/odata/SAP/zodata_porject_srv/";
+			var oModel = new sap.ui.model.odata.v2.ODataModel(url, {
+				json: true
+			});
+			oModel.setUseBatch(false);
+			this.getView().setModel(oModel);
+			//this.byId("listid").bindItems(oModel);
+			//oModel.setSizeLimit(10);
+		}
+	});
+});
